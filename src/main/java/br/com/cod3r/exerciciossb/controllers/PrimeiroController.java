ad7f8f689e661340a4034188a8bc8988929d9953
rbverissimo <rverissimo.dev@gmail.com>
2023-02-14 16:14:09 -0300

    Compreendedo que o path do @GetMapping é um array, portanto é possível passar vários parâmetros de endpoints para ele

--- src/main/java/br/com/cod3r/exerciciossb/controllers/PrimeiroController.java
+++ src/main/java/br/com/cod3r/exerciciossb/controllers/PrimeiroController.java
@@ -1,13 +1,13 @@
 package br.com.cod3r.exerciciossb.controllers;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class PrimeiroController {
 	
-	@RequestMapping(method = RequestMethod.GET, path = "/ola")
+	// @RequestMapping(method = RequestMethod.GET, path = "/ola")
+	@GetMapping(path = {"/ola", "/saudacao"})
 	public String ola() {
 		return "Olá, Spring!";
 		
