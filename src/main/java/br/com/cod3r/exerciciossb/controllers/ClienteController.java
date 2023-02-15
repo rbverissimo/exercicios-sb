954bee62d18a3073bfc47fa8a1506b58e25240d6
rbverissimo <rverissimo.dev@gmail.com>
2023-02-14 22:31:30 -0300

    Usando o @PathVariable para definir o valor passado pela URL

--- src/main/java/br/com/cod3r/exerciciossb/controllers/ClienteController.java
+++ src/main/java/br/com/cod3r/exerciciossb/controllers/ClienteController.java
@@ -1,6 +1,7 @@
 package br.com.cod3r.exerciciossb.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,6 +14,11 @@ public class ClienteController {
 	@GetMapping(path = "/qualquer")
 	public Cliente obterCliente() {
 		return new Cliente(28, "Pedro", "123.456.789-00");
+	}
+	
+	@GetMapping("/{id}")
+	public Cliente obterClientePorId1(@PathVariable int id) {
+		return new Cliente(id, "Maria", "987.654.321.-00");
 	}
 
 }
