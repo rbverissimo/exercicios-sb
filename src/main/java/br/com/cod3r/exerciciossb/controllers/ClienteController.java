43dcdf41cb56258a2620b0a35c7bb17954220129
rbverissimo <rverissimo.dev@gmail.com>
2023-02-14 22:41:20 -0300

    Implementando a segunda maneira de passar parametros através do RequestParam

--- src/main/java/br/com/cod3r/exerciciossb/controllers/ClienteController.java
+++ src/main/java/br/com/cod3r/exerciciossb/controllers/ClienteController.java
@@ -3,6 +3,7 @@ package br.com.cod3r.exerciciossb.controllers;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.cod3r.exerciciossb.models.Cliente;
@@ -19,6 +20,12 @@ public class ClienteController {
 	@GetMapping("/{id}")
 	public Cliente obterClientePorId1(@PathVariable int id) {
 		return new Cliente(id, "Maria", "987.654.321.-00");
+	}
+	
+	@GetMapping
+	public Cliente obterClientePorId2(
+			@RequestParam(name= "id") int id) {
+		return new Cliente(id, "João Augusto", "111.222.333-44");
 	}
 
 }
