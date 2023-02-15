f4784e61e03d2b36b9c70fd9cb109423a7472f92
rbverissimo <rverissimo.dev@gmail.com>
2023-02-14 21:23:03 -0300

    Implementado um formulario simples em HTML para demonstrar o uso método do POST diretamente no navegador

--- src/main/java/br/com/cod3r/exerciciossb/controllers/MetodosHttpController.java
+++ src/main/java/br/com/cod3r/exerciciossb/controllers/MetodosHttpController.java
@@ -5,9 +5,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/metodos")
 public class MetodosHttpController {
 	
 	@GetMapping
