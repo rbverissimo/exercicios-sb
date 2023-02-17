7f6d24702a25df1f0a0be94df2cd4bb0edef48c4
rbverissimo <rverissimo.dev@gmail.com>
2023-02-16 22:37:11 -0300

    Definindo a conexão com o banco de dados no application.properties. Implementando a dependência ao JPA no POM XML e implementando a classe Produto como uma @Entity do JPA. Usando o padrão derivado do DDD de Repository para criar o CrudRepository ligado ao Produto.

---
+++ src/main/java/br/com/cod3r/exerciciossb/model/entities/Produto.java
@@ -0,0 +1,35 @@
+package br.com.cod3r.exerciciossb.model.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Produto {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	private String nome;
+	
+	public Produto() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	
+
+}
