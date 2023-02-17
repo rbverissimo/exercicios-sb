3c75877a0d627441e009e2de838c0ccca12797fb
rbverissimo <rverissimo.dev@gmail.com>
2023-02-17 00:30:27 -0300

    Implementação da injeção de dependência através do @AutoWired

--- src/main/java/br/com/cod3r/exerciciossb/controllers/ProdutoController.java
+++ src/main/java/br/com/cod3r/exerciciossb/controllers/ProdutoController.java
@@ -1,19 +1,26 @@
 package br.com.cod3r.exerciciossb.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.cod3r.exerciciossb.model.entities.Produto;
+import br.com.cod3r.exerciciossb.model.repositories.ProdutoRepository;
 
 @RestController
 @RequestMapping("/api/produtos")
 public class ProdutoController {
 	
+	@Autowired
+	private ProdutoRepository produtoRepository;
+	
 	@PostMapping
-	public Produto novoProduto(@RequestParam String nome) {
+	public @ResponseBody Produto novoProduto(@RequestParam String nome) {
 		Produto p = new Produto(nome);
+		produtoRepository.save(p);
 		return p;
 	}
 
