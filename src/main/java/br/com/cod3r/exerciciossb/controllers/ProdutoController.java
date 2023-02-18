6039b56d2eb68db306eb3cb0acdc1d17953c5d70
rbverissimo <rverissimo.dev@gmail.com>
2023-02-18 12:57:46 -0300

    Implementando o obterProdutosPorId usando o findById() e passando o id. Esse método do Repository retorna um Optional de Produto.

--- src/main/java/br/com/cod3r/exerciciossb/controllers/ProdutoController.java
+++ src/main/java/br/com/cod3r/exerciciossb/controllers/ProdutoController.java
@@ -1,7 +1,10 @@
 package br.com.cod3r.exerciciossb.controllers;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -30,6 +33,9 @@ public class ProdutoController {
 		return produtoRepository.findAll();
 	}
 	
-	
+	@GetMapping(path="/{id}")
+	public Optional<Produto> obterProdutoPorId(@PathVariable int id) {
+		return produtoRepository.findById(id);
+	}
 
 }
