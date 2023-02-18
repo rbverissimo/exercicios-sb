d2d870d8a892ea32beb335aaecb2e19f22654254
rbverissimo <rverissimo.dev@gmail.com>
2023-02-18 12:53:14 -0300

    Implementando o findAll() que retorna um iterável para obter TODOS os produtos da tabela Produto.

--- src/main/java/br/com/cod3r/exerciciossb/controllers/ProdutoController.java
+++ src/main/java/br/com/cod3r/exerciciossb/controllers/ProdutoController.java
@@ -1,6 +1,7 @@
 package br.com.cod3r.exerciciossb.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -22,6 +23,11 @@ public class ProdutoController {
 	public @ResponseBody Produto novoProduto( @Valid Produto p) {
 		produtoRepository.save(p);
 		return p;	
+	}
+	
+	@GetMapping
+	public Iterable<Produto> obterProdutos() {
+		return produtoRepository.findAll();
 	}
 	
 	
