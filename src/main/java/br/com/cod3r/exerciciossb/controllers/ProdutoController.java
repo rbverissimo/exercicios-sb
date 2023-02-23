bcaf94ccbc372956c8eae92c2332ca25ff053e3c
rbverissimo <rverissimo.dev@gmail.com>
2023-02-22 23:56:59 -0300

    Implementado o método de @DeleteMapping para deletar por id

--- src/main/java/br/com/cod3r/exerciciossb/controllers/ProdutoController.java
+++ src/main/java/br/com/cod3r/exerciciossb/controllers/ProdutoController.java
@@ -3,10 +3,9 @@ package br.com.cod3r.exerciciossb.controllers;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -44,5 +43,10 @@ public class ProdutoController {
 	 * @PutMapping public Produto alterarProduto( @Valid Produto p) {
 	 * produtoRepository.save(p); return p; }
 	 */
+	
+	@DeleteMapping(path="/{id}")
+	public void excluirProduto(@PathVariable int id) {
+		produtoRepository.deleteById(id);
+	}
 
 }
