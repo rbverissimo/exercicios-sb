00544fa675a111a6b4b8033071445d4c4ddc1e44
rbverissimo <rverissimo.dev@gmail.com>
2023-02-23 15:03:35 -0300

    Implementada a consultada pagina usando a interface PagingAndSorintgRepository que, do Spring 3+ deixou de ser acoplada ao CrudRepository, portanto é necessário extender as duas interfaces dentro do Repository para que elas tenham o funcionamento ideal

--- src/main/java/br/com/cod3r/exerciciossb/controllers/ProdutoController.java
+++ src/main/java/br/com/cod3r/exerciciossb/controllers/ProdutoController.java
@@ -3,6 +3,8 @@ package br.com.cod3r.exerciciossb.controllers;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -32,6 +34,13 @@ public class ProdutoController {
 	@GetMapping
 	public Iterable<Produto> obterProdutos() {
 		return produtoRepository.findAll();
+	}
+	
+	@GetMapping(path = "/pagina/{numeroPagina}")
+	public Iterable<Produto> obterProdutosPorPagina(
+			@PathVariable int numeroPagina){
+		Pageable page = PageRequest.of(numeroPagina, 3);
+		return produtoRepository.findAll(page);
 	}
 	
 	@GetMapping(path="/{id}")
--- src/main/java/br/com/cod3r/exerciciossb/model/repositories/ProdutoPagingRepository.java
+++ src/main/java/br/com/cod3r/exerciciossb/model/repositories/ProdutoPagingRepository.java
@@ -1,11 +1,10 @@
 package br.com.cod3r.exerciciossb.model.repositories;
 
-import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
 import br.com.cod3r.exerciciossb.model.entities.Produto;
 
-public interface ProdutoRepository extends 
-	PagingAndSortingRepository<Produto, Integer>, CrudRepository<Produto, Integer>{
+public interface ProdutoPagingRepository extends 
+	PagingAndSortingRepository<Produto, Integer> {
 
 }
